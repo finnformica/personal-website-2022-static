f510a004012a3c4d7c195fc17a7c4d8cc0f1830b
finnformica <42536816+finnformica@users.noreply.github.com>
2022-06-07 12:26:45 +0100

    fixed bug with theme toggle

--- static/theme.js
+++ static/theme.js
@@ -4,23 +4,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const storedTheme = localStorage.getItem('theme');
 
     if (storedTheme) {
-        themeStylesheet.href = storedTheme;
+      if (storedTheme != themeStylesheet.href) {
+        loadTheme();
+      }
     }
 
     const themeToggle = document.getElementById('theme-toggle');
 
-    themeToggle.addEventListener('click', () => {
-
-
-        if (themeStylesheet.href.includes('light')) {
-            // if it's light -> go dark
-            themeStylesheet.href = 'static/css/dark-theme.css';
-
-        } else {
-            // if it's dark -> go light
-            themeStylesheet.href = 'static/css/light-theme.css';
-        }
-        // save the preference to localStorage
-        localStorage.setItem('theme',themeStylesheet.href)
-    })
+    themeToggle.addEventListener('click', loadTheme)
 })
+
+
+function loadTheme() {
+
+  const themeStylesheet = document.getElementById('theme');
+
+  if (themeStylesheet.href.includes('light')) {
+      // if it's light -> go dark
+      themeStylesheet.href = 'static/css/dark-theme.css';
+      navbar = document.getElementById("navbar");
+      navbar.classList.remove("navbar-light");
+      navbar.classList.add("navbar-dark");
+
+  } else {
+      // if it's dark -> go light
+      themeStylesheet.href = 'static/css/light-theme.css';
+      navbar = document.getElementById("navbar");
+      navbar.classList.add("navbar-light");
+      navbar.classList.remove("navbar-dark");
+  }
+  // save the preference to localStorage
+  localStorage.setItem('theme',themeStylesheet.href)
+
+}
